6e9d61fa0ba4959ddc3b9246647d20d2cee367b2
4ksi <naqsee@gmail.com>
2016-01-22 02:38:16 +0300

    added output news from database

--- app/models/model_news.php
+++ app/models/model_news.php
@@ -1,23 +1,15 @@
 <?php
 class Model_News extends Model
 {
-    public function get_data()
-    {   
-        return array(
-            array(
-                'Title' => 'Lorem #1',
-                'Date' => '12.12.2012',
-                'Anounce' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae libero sapiente labore inventore...',
-                'Full' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque temporibus explicabo reprehenderit, id quo? Aperiam aliquam odit quisquam odio, sit facilis perspiciatis omnis aut illum eveniet eaque, quas deserunt eligendi.'
-            ),
-            array(
-                'Title' => 'Ipsum #2',
-                'Date' => '12.12.2012',
-                'Anounce' => 'Explicabo reprehenderit, id quo? Aperiam aliquam odit quisquam odio, sit facilis perspiciatis omnis Aperiam aliquam odit quisquam odio, sit facilis perspiciatis omnis Aperiam aliquam odit quisquam odio, sit facilis perspiciatis omnis Aperiam aliquam odit quisquam odio, sit facilis perspiciatis omnis ',
-                'Full' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque temporibus explicabo reprehenderit, id quo? Aperiam aliquam odit quisquam odio, sit facilis perspiciatis omnis aut illum eveniet eaque, quas deserunt eligendi.'
-            ),
-            // todo
-        );
+    public function get()
+    {
+        $connect = self::open_connection();
+        $result = $connect->query('SELECT id, title, announce, created_at '
+                . 'FROM news_db');
+
+        return $result;
+
+        self::close_connection($connect);
     }
 }
 ?>
