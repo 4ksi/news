1bcb0dd890aa07e4c4966be7fb1e5248ab69c9b5
4ksi <naqsee@gmail.com>
2016-01-21 06:13:00 +0300

    optimizing

--- app/views/news_view.php
+++ app/views/news_view.php
@@ -2,14 +2,22 @@
     <h1 class="text-center">Последние новости</h1>
 </div>
 <div class="container-fluid">
-<?php
-    foreach($data as $row) {
-        echo "<div class='row'><h2 class='text-uppercase'>{$row['Title']} <small>{$row['Date']}</small></h3></div>";
-        echo "<div class='row'><p class='lead text-justify'> {$row['Anounce']} </p></div>";
-        echo "<div class='btn-group' role='group'>";
-        echo "<button type='button' class='btn btn-default'>EDIT</button>";
-        echo "<button type='button' class='btn btn-default'>DELETE</button>";
-        echo "</div>";
-    }
-?>
+    <?php foreach($data as $row): ?>
+            <div class='row'>
+                <h2 class='text-uppercase'><?= $row['title'] ?>
+                    <small><?= $row['created_at'] ?></small>
+                </h2>
+            </div>
+
+            <div class='row'>
+                <p class='lead text-justify'><?= $row['announce'] ?></p>
+            </div>
+
+            <div class='row'>
+                <div class='btn-group' role='group'>
+                    <button type='button' class='btn btn-default'>EDIT</button>
+                    <button type='button' class='btn btn-default'>DELETE</button>
+                </div>
+            </div>
+    <?php endforeach ?>
 </div>
