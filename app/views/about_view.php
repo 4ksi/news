7bd35e7c9826f63a372040e768b8804ae5c19843
4ksi <naqsee@gmail.com>
2016-01-21 06:11:02 +0300

    Add about info

---
+++ app/views/about_view.php
@@ -0,0 +1,11 @@
+<div class="page-header">
+    <h1 class="text-left">О сайте</h1>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <blockquote>
+          <p>Общество испытывает поистине ненасытное любопытство ко всему, любопытства не заслуживающему.</p>
+          <footer>Оскар Уайльд</footer>
+        </blockquote>
+    </div>
+</div>
