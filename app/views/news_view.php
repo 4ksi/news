6208b3abb9ee3eb3805ab8c47accea6f92395e67
4ksi <naqsee@gmail.com>
2016-01-21 04:47:01 +0300

    Buttons added. Edit page-header

--- app/views/news_view.php
+++ app/views/news_view.php
@@ -1,16 +1,15 @@
-<div class="container-fluid">
-    <div class="row">
-        <h1 class="text-center">Последние новости</h1>
-    </div>
+<div class="page-header">
+    <h1 class="text-center">Последние новости</h1>
 </div>
 <div class="container-fluid">
-  
-    ...
-  
 <?php
     foreach($data as $row) {
-        echo "<div class='row'><h3>{$row['Title']} <small>{$row['Date']}</small></h3></div>";
-        echo "<div class='row'> {$row['Anounce']} </div>";
+        echo "<div class='row'><h2 class='text-uppercase'>{$row['Title']} <small>{$row['Date']}</small></h3></div>";
+        echo "<div class='row'><p class='lead text-justify'> {$row['Anounce']} </p></div>";
+        echo "<div class='btn-group' role='group'>";
+        echo "<button type='button' class='btn btn-default'>EDIT</button>";
+        echo "<button type='button' class='btn btn-default'>DELETE</button>";
+        echo "</div>";
     }
 ?>
 </div>
