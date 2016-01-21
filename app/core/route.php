6678d041e7106d95bbb4cc62b2ca0e6d4a1acd00
4ksi <naqsee@gmail.com>
2016-01-22 02:28:54 +0300

    small fixes (commentaries and codestyle)

--- app/core/route.php
+++ app/core/route.php
@@ -3,63 +3,62 @@ class Route
 {
     static function start()
     {
-        // контроллер и действие по умолчанию
-        $controller_name = 'news';
-        $action_name = 'index';
-        
+        // Контроллер и действие по умолчанию
+        $controller_name    = 'news';
+        $action_name        = 'index';
+
+
+        // Разбиваем url
         $routes = explode('?', $_SERVER['REQUEST_URI']);
 
-        // получаем имя контроллера
-        if ( !empty($routes[1]) )
-        {   
+        // Получаем имя контроллера
+        if (!empty($routes[1])) {
             $controller_name = $routes[1];
         }
         
-        // получаем имя экшена
-        if ( !empty($routes[2]) )
-        {
+        // Получаем имя экшена
+        if (!empty($routes[2])) {
             $action_name = $routes[2];
         }
 
-        // добавляем префиксы
-        $model_name = 'model_'.$controller_name;
-        $controller_name = 'controller_'.$controller_name;
-        $action_name = 'action_'.$action_name;
 
-        // подцепляем файл с классом модели (файла модели может и не быть)
+        // Добавляем префиксы
+        $model_name         = 'model_'.$controller_name;
+        $controller_name    = 'controller_'.$controller_name;
+        $action_name        = 'action_'.$action_name;
+
+
+        // Подцепляем файл с классом модели (файла модели может и не быть)
         $model_file = strtolower($model_name).'.php';
         $model_path = "app/models/".$model_file;
-        if(file_exists($model_path))
-        {
+        // Если файл существует - подключаем
+        if (file_exists($model_path)) {
             include "app/models/".$model_file;
         }
 
-        // подцепляем файл с классом контроллера
+
+        // Подцепляем файл с классом контроллера
         $controller_file = strtolower($controller_name).'.php';
         $controller_path = "app/controllers/".$controller_file;
-        if(file_exists($controller_path))
-        {
+
+        // Если файл существует - подключаем
+        if (file_exists($controller_path)) {
             include "app/controllers/".$controller_file;
-        }
-        else
-        {
+        } else {
             // TODO: THROW EXCEPTION
         }
-        
-        // создаем контроллер
+
+
+        // Создаем контроллер
         $controller = new $controller_name;
         $action = $action_name;
-        
-        if(method_exists($controller, $action))
-        {
-            // вызываем действие контроллера
+
+        // Если метод существует - вызываем
+        if (method_exists($controller, $action)) {
             $controller->$action();
-        }
-        else
-        {
+        } else {
             // TODO: THROW EXCEPTION
         }
     }
-    
 }
 ?>
