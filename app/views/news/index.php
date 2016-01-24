e48d6a8a9a6a0431310986736244f099a7819463
4ksi <naqsee@gmail.com>
2016-01-24 21:00:30 +0300

    fix layout

--- app/views/news/index.php
+++ app/views/news/index.php
@@ -1,30 +1,34 @@
-<div class='container-fluid'>
-    <div class='page-header'>
-        <h1 class='text-center'>Последние новости</h1>
+<div class="container-fluid">
+    <div class="page-header">
+        <div class="row">
+            <h1 class="text-center">Последние новости</h1>
+        </div>
     </div>
-    <div class='container-fluid'>
-        <?php foreach ($data as $newsItem): ?>
-            <div class='row'>
-                <h2 class='text-uppercase'>
-                    <a href="/news/view/<?=$newsItem['id']?>"><?= $newsItem['title'] ?></a>
-                    <small><?= $newsItem['created_at'] ?></small>
-                </h2>
-            </div>
+    <?php foreach ($data as $newsItem): ?>
+        <div class="row">
+            <h2 class="text-uppercase">
+                <a href="/news/view/<?=$newsItem['id']?>"><?= $newsItem['title'] ?></a>
+                <small><?= $newsItem['created_at'] ?></small>
+            </h2>
+        </div>
 
-            <div class='row'>
-                <p class='lead text-justify'><?= $newsItem['announce'] ?><a href="/news/view/<?=$newsItem['id']?>">...continue</a></p>
-            </div>
+        <div class="row">
+            <p class="lead text-justify"><?= $newsItem['announce'] ?> ...</p>
+        </div>
 
-            <div class='row'>
-                <div class='btn-group' role='group'>
-                    <button type='button' class='btn btn-default'>
-                        <a href="edit/<?= $newsItem['id'] ?>">EDIT</a>
-                    </button>
-                    <button type='button' class='btn btn-default'>
-                        <a href="delete/<?= $newsItem['id'] ?>">DELETE</a>
-                    </button>
-                </div>
+        <div class="'row">
+            <div class="btn-group" role="group">
+                <a class="btn btn-default" href="/news/edit/<?= $newsItem['id'] ?>">
+                    EDIT
+                </a>
+                <a class="btn btn-default" href="/news/delete/<?= $newsItem['id'] ?>">
+                    DELETE
+                </a>
             </div>
-        <?php endforeach ?>
+        </div>
+    <?php endforeach ?>
+
+    <div class="pull-right">
+        <a style="position:fixed; right: 100px; bottom:30px;" href="" class="btn btn-success">Добавить новость</a>
     </div>
 </div>
