f1d3f524479608562653b1a4e67d835bf373e5b8
4ksi <naqsee@gmail.com>
2016-01-25 01:10:13 +0300

    "add" route

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,8 +2,12 @@
 return array(
     'news/([0-9]+)'         => 'news/view/$1',
     'news'                  => 'news/index',
+    'news/add'              => 'news/add',
     'news/edit/([0-9]+)'    => 'news/edit/$1',
     'news/delete/([0-9]+)'  => 'news/delete/$1',
 
-    'about'           => 'about/index',
+    'about'                 => 'about/index',
+
+    'index.php'             => 'news/index',
+    ''                      => 'news/index',
 );
