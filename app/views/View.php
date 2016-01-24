500ce3f3bd8d2cd1d4a29206a7ebe95d5b24dcd9
4ksi <naqsee@gmail.com>
2016-01-24 18:30:42 +0300

    smallfix

--- app/views/View.php
+++ app/views/View.php
@@ -3,6 +3,6 @@ class View
 {
     function generate($content_view, $template_view, $data = null)
     {
-        include 'app/views/'.$template_view;
+        include_once 'app/views/'.$template_view;
     }
 }
