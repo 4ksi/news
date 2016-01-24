1a59d028e85835452aecd26e9f120bed169dc49d
4ksi <naqsee@gmail.com>
2016-01-24 21:04:52 +0300

    Add few buttons: Back (To the main page); Edit; Delete.

--- app/views/news/view.php
+++ app/views/news/view.php
@@ -1,17 +1,27 @@
 <div class="container-fluid">
     <div class="page-header">
-        <h1 class="text-center"><?=$data['title'] ?>
-            <small><?= $data['created_at'] ?></small>
-        </h1>
-    </div>
-    <div class="container-fluid">
-            <div class='row'>
-                <h2 class='text-uppercase'>
-                </h2>
-            </div>
+        <div class="row">
+            <h1 class="text-center text-uppercase"><?=$data['title'] ?>
+                <small><?= $data['created_at'] ?></small>
+            </h1>
+        </div>
+        <div class="row">
 
-            <div class='row'>
-                <p class='lead text-justify'><?= $data['full_news'] ?></p>
+            <a class="btn btn-primary" href="/news/">Назад</a>
+
+            <div class="btn-group pull-right">
+                <a class="btn btn-default" href="/news/edit/<?=$data['id']?>">
+                    <span class="glyphicon glyphicon-pencil">Изменить</span>
+                </a>
+                <a class="btn btn-default"  href="/news/delete/<?=$data['id']?>">
+                    <span class="glyphicon glyphicon-remove">Удалить</span>
+                </a>
             </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <p class="lead text-justify"><?= $data['full_news'] ?></p>
     </div>
 </div>
