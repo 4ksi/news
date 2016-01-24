96248b4efe6476403782a3b7fd781bc89230ca87
4ksi <naqsee@gmail.com>
2016-01-24 08:08:04 +0300

    Fixes

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // Подключаем компоненты
 define('ROOT', dirname(__FILE__));
-require_once ROOT . '/app/load.php';
+require_once ROOT . '/app/components/Router.php';
+require_once ROOT.  '/app/components/Db.php';
+require_once ROOT.  '/app/views/View.php';
 
 // Вызываем роутер
 $router = new Router();
 $router->start();
-
-?>
