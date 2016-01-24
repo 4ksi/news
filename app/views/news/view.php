1b51dd4ba1c4751ed8a9f949be3018e4f8e8aecc
4ksi <naqsee@gmail.com>
2016-01-24 08:09:11 +0300

    view page for every news

---
+++ app/views/news/view.php
@@ -0,0 +1,17 @@
+<div class="container-fluid">
+    <div class="page-header">
+        <h1 class="text-center"><?=$data['title'] ?>
+            <small><?= $data['created_at'] ?></small>
+        </h1>
+    </div>
+    <div class="container-fluid">
+            <div class='row'>
+                <h2 class='text-uppercase'>
+                </h2>
+            </div>
+
+            <div class='row'>
+                <p class='lead text-justify'><?= $data['full_news'] ?></p>
+            </div>
+    </div>
+</div>
