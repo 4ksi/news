ea796fef2b7a5f598c66dbfcea24950e37edca3f
aksi <naqsee@gmail.com>
2016-04-09 06:48:14 +0300

    Autoload LOADING with composer

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-// Подключаем автолоадер классов
 define('ROOT', dirname(__FILE__));
-require_once ROOT . '/app/components/Autoload.php';
 
 // Вызываем роутер
 $router = new Router();
